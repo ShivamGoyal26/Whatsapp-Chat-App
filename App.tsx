7d514089f453740b02a69e0c8ec2a716d79b341c
ShivamGoyal26 <officialshivamgoyal@gmail.com>
2021-01-29 11:02:00 +0530

    Amplify has been added

--- App.tsx
+++ App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import RootNavigator from './Navigation/index';
 
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+Amplify.configure(config)
+
 const MyApp = props => {
   return (
     
